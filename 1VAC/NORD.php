ae2d1068993a098694d5f7a3f57f9ab537881275
tibaredha <tibaredha@yahoo.fr>
2020-11-05 19:08:18 +0100

    maj

--- 1VAC/NORD.php
+++ 1VAC/NORD.php
@@ -36,9 +36,10 @@ echo"<hr>";echo"<BR>";
 $ESPECE=array("Ovine","Caprine","Bovine","Equine","Canine","Feline","Cameline","Repro_Chair","Repro_Ponte","Pondeuse","Poulet_Chair","Dinde","Repro_Dinde","Pigeons","Oiseaux","Poisson","Autre");
 $per ->label(80,340+40,'* Espèce');$per ->combov(200,340+40,'ESPECE',$ESPECE)  ;
 
+$Sexe=array("M","F");
+$per ->label(80+800,340+40,'* Sexe');$per ->combov(200+800,340+40,'SEXE',$Sexe)  ;
 
-
-
+echo"<BR>";echo"<hr>";
 $per ->hide(10,10,'WIL',20,$_SESSION["WILAYA"]);
 $per ->hide(10,10,'DAI',20,$_SESSION["DAIRA"]);
 $per ->hide(10,10,'COM',20,$_SESSION["COMMUNE"]);
